f789146ddda8043f7830fe4cb1be9da12d0d40c7
novin997 <cocolupiayi@gmail.com>
2020-08-29 16:43:05 +0800

    axios and basic component

--- vue3-test/src/router/index.ts
+++ vue3-test/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import Home from "../views/Home.vue";
+import Composition from "../views/Composition.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -15,6 +16,11 @@ const routes: Array<RouteRecordRaw> = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/composition",
+    name: "Composition",
+    component: Composition
   }
 ];
 
---
+++ vue3-test/src/views/Composition.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <button @click="add">test</button>
+    <div>{{ state.data }}</div>
+    <Card />
+  </div>
+</template>
+
+<script lang="ts">
+import { reactive, onMounted } from "vue";
+import axios from "axios";
+import Card from "../components/Card.vue";
+
+export default {
+  components: {
+    Card,
+  },
+  setup() {
+    const state = reactive({
+      count: 0,
+      data: [],
+    });
+
+    function add() {
+      state.count++;
+    }
+
+    async function getData() {
+      const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+      state.data = res.data;
+    }
+
+    onMounted(() => {
+      getData();
+    });
+
+    return {
+      state,
+      add,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+button {
+  margin: 5px;
+}
+</style>
