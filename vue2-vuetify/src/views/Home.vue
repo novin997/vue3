4cda9654de4896b7dc9fa713e17cce292ec82623
novin997 <cocolupiayi@gmail.com>
2020-09-05 20:50:58 +0800

    update image

--- vue2-vuetify/src/views/Home.vue
+++ vue2-vuetify/src/views/Home.vue
@@ -1,6 +1,15 @@
 <template>
-  <v-carousel cycle heigth="400">
-    <v-carousel-item v-for="(slide, i) in state.slides" :key="i"></v-carousel-item>
+  <v-carousel hide-delimiters cycle heigth="800">
+    <v-carousel-item 
+      v-for="(image, i) in state.images" 
+      :key="i"
+    >
+      <v-img :src="image.src">
+        <v-row class="fill-height" align="center" justify="center">
+          <div class="display-3 white--text">Hello</div>
+        </v-row>
+      </v-img>
+    </v-carousel-item>
   </v-carousel>
 </template>
 
@@ -9,12 +18,17 @@
 <script lang="ts">
 import { ref, defineComponent } from "@vue/composition-api";
 
+declare let require: any;
+
 export default defineComponent({
   setup() {
     const state = ref<object>({
-      slides: ["1", "2", "3", "4", "5"],
+      images: [
+        {
+          src: require("../assets/K-On.jpg")
+        },
+      ],
     });
-
     return {state};
   },
 });
